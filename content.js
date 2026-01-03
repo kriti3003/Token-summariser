20b6ce4e56c79938aa6c93702e3544c4ade404da
kriti3003 <83621556+kriti3003@users.noreply.github.com>
2026-01-03 18:10:56 +0530

    movable

--- content.js
+++ content.js
@@ -102,6 +102,44 @@ function showSummaryModal(tokenCount, messages) {
     });
 }
 
+// Make element draggable
+function makeDraggable(element) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  element.onmousedown = dragMouseDown;
+  element.style.cursor = "move";
+
+  function dragMouseDown(e) {
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // Set new position
+    element.style.top = element.offsetTop - pos2 + "px";
+    element.style.left = element.offsetLeft - pos1 + "px";
+    element.style.bottom = "auto";
+    element.style.right = "auto";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 // Create and update token counter display
 function updateTokenDisplay(tokens) {
   let display = document.getElementById("token-counter-display");
@@ -110,6 +148,9 @@ function updateTokenDisplay(tokens) {
     display = document.createElement("div");
     display.id = "token-counter-display";
     document.body.appendChild(display);
+
+    // Make the display draggable
+    makeDraggable(display);
   }
 
   const percentage = (tokens / 200000) * 100;
@@ -129,6 +170,9 @@ function updateTokenDisplay(tokens) {
       <div class="token-counter-target">Target: 200,000</div>
     </div>
   `;
+
+  // Reattach drag functionality after innerHTML update
+  makeDraggable(display);
 }
 
 // Main monitoring function
@@ -222,7 +266,7 @@ function init() {
       } catch (error) {
         console.error("Token monitoring error:", error);
       }
-    }, 10000);
+    }, 200000);
   } catch (error) {
     console.error("Extension initialization error:", error);
   }
